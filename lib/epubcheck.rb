8934aabea3cfc2d20e666b78927b0612cba8adb9
tbelc <34096219+tbelc@users.noreply.github.com>
2019-09-19 12:11:49 -0400

    Updated epubcheck to accept both a directory or archive.

--- lib/epubcheck.rb
+++ lib/epubcheck.rb
@@ -1,16 +1,18 @@
 class EpubCheck
   JAR_PATH = File.join(__dir__, 'jars', 'epubcheck-jar-with-dependencies.jar')
-  CMD_STR = "java -jar \"%s\" -q -o \"%s\" \"%s\""
+  CMD_STR_EPUB = "java -jar \"%s\" -q -o \"%s\" \"%s\""
+  CMD_STR_EPUBDIR = "java -jar \"%s\" -q -o \"%s\" -mode exp \"%s\""
 
   def self.check_file(args)
-    epub_file = args[:epubfile]
+    epub_path = args[:epubpath]
     log_file = args[:logfile]
 
-    cmd_str = sprintf(CMD_STR, JAR_PATH, log_file, epub_file)
-    puts cmd_str
+    cmd_str = File.directory?(epub_path) ? CMD_STR_EPUBDIR : CMD_STR_EPUB
+    java_cmd_str = sprintf(cmd_str, JAR_PATH, log_file, epub_path)
+    puts java_cmd_str
     STDOUT.flush
 
-    ok = system(cmd_str)
+    ok = system(java_cmd_str)
     status = $?
 
     case ok
