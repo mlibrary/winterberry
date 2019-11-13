d7b6a662e57c07a4a67d34fa80448bf4bcc894d1
tbelc <34096219+tbelc@users.noreply.github.com>
2019-11-13 12:36:16 -0500

    Added simple/full interface for epubcheck.

--- lib/epubcheck.rb
+++ lib/epubcheck.rb
@@ -1,14 +1,25 @@
 class EpubCheck
-  JAR_PATH = File.join(__dir__, 'jars', 'epubcheck-jar-with-dependencies.jar')
-  CMD_STR_EPUB = "java -jar \"%s\" -q -o \"%s\" \"%s\""
-  CMD_STR_EPUBDIR = "java -jar \"%s\" -q -o \"%s\" -mode exp \"%s\""
+  JAR_PATH = File.join(__dir__, 'epubcheck', 'epubcheck.jar')
+  CMD_STR_EPUB = "-q -o \"%s\" \"%s\""
+  CMD_STR_EPUBDIR = "-q -o \"%s\" -mode exp \"%s\""
+  CMD_STR_ALL = "java -jar \"%s\" %s"
 
   def self.check_file(args)
     epub_path = args[:epubpath]
     log_file = args[:logfile]
 
     cmd_str = File.directory?(epub_path) ? CMD_STR_EPUBDIR : CMD_STR_EPUB
-    java_cmd_str = sprintf(cmd_str, JAR_PATH, log_file, epub_path)
+    java_cmd_str = sprintf(cmd_str, log_file, epub_path)
+    execute(java_cmd_str)
+  end
+
+  def self.invoke(args = [])
+    opts_str = args.count == 0 ? "-h" : args.join(' ')
+    execute(opts_str)
+  end
+
+  def self.execute(opts_str)
+    java_cmd_str = sprintf(CMD_STR_ALL, JAR_PATH, opts_str)
     puts java_cmd_str
     STDOUT.flush
 
@@ -21,4 +32,5 @@ class EpubCheck
       #puts "epubcheck failed (status = #{status})"
     end
   end
+
 end
