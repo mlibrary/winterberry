51cc73d2f6b883c7ed5bf04afebd75f822b1e2e4
tbelc <34096219+tbelc@users.noreply.github.com>
2019-12-09 13:54:07 -0500

    Update xpath expression to handle different resource markup.

--- lib/resources/resourceprocessor.rb
+++ lib/resources/resourceprocessor.rb
@@ -31,6 +31,7 @@ class ResourceProcessor
 	def resources(doc)
 		#doc.xpath("//*[@class='fig' or @class='rb']")
 		#doc.xpath("//*[@class='fig']") + doc.xpath("//*[@class='rb']")
-		doc.xpath("//*[local-name()='figure' or @class='fig' or @class='rb']")
+		#doc.xpath("//*[local-name()='figure' or @class='fig' or @class='rb']")
+		doc.xpath("//*[(local-name()='p' and @class='fig') or (local-name()='figure' and count(*[local-name()='p' and @class='fig'])=0) or @class='rb']")
 	end
 end
