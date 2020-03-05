1110834095bbb028ea824393acc0fec9c32110b8
tbelc <34096219+tbelc@users.noreply.github.com>
2020-03-05 13:32:11 -0500

    Update caption messages for reviewing figures. Modify update_fmsl to use monograph directory instead of isbn.

--- lib/review/figureprocessor.rb
+++ lib/review/figureprocessor.rb
@@ -22,10 +22,16 @@ class FigureProcessor < ReviewProcessor
               :content => fragment.node.to_xml
           )
 
-      fragment.has_elements.each do |key, exists|
-        fragment.review_msg_list << "Figure INFO:     has <#{key}>" if exists
-        fragment.review_msg_list << "Figure Warning:  has no <#{key}>" unless exists
+      caption_elem = ""
+      fragment.has_elements.each do |elem_name, exists|
+        if exists
+          fragment.review_msg_list << "Figure INFO:           has #{elem_name}"
+          caption_elem = elem_name if caption_elem.empty?
+        end
       end
+      msg = "Figure INFO:           has caption (#{caption_elem})" unless caption_elem.empty?
+      msg = "Figure Warning:        has no caption" if caption_elem.empty?
+      fragment.review_msg_list << msg
 
       img_fragments.each do |img_frag|
         img_frag.review_msg_list.each do |msg|
