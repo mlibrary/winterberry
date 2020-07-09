0ec5da87fc26650b5582b3af4f0236b76cd9a836
tbelc <34096219+tbelc@users.noreply.github.com>
2020-07-09 09:45:54 -0400

    Fix issue with locating entries within the EPUB due to location of OPF file.

--- lib/epub.rb
+++ lib/epub.rb
@@ -39,8 +39,11 @@ class Epub
         item = @manifest.node.xpath(".//*[local-name()='item' and @id=\"#{idref}\"]").first
         raise "Error: finding manifest item #{idref}" if item.nil?
 
-        #item_entry = @epub.glob(File.join(opf_dir, item['href'])).first
-        item_entry = @epub.glob(item['href']).first
+        # This sometimes fails, but not sure why the path
+        # within the epub should vary depending on whether
+        # the OPF is in the same directory or not.
+        item_entry = @epub.glob(File.join(opf_dir, item['href'])).first
+        #item_entry = @epub.glob(item['href']).first
         raise "Error: loading manifest item #{item['href']}" if item_entry.nil?
 
         @spine_items << item_entry
