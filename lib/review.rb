ab30c3dcb51a3a52ddc1e633d3b5ee9d280dac27
tbelc <34096219+tbelc@users.noreply.github.com>
2020-09-30 12:05:24 -0400

    Modified review_epub script to support EPUB version, and navigation file.

--- lib/review.rb
+++ lib/review.rb
@@ -1,13 +1,14 @@
 module UMPTG
   require_relative 'fragment'
 
-  require_relative File.join('review', 'reviewinfo')
+  require_relative File.join('review', 'reviewobject')
   require_relative File.join('review', 'reviewprocessor')
   require_relative File.join('review', 'figureprocessor')
   require_relative File.join('review', 'imgprocessor')
   require_relative File.join('review', 'linkprocessor')
   require_relative File.join('review', 'listprocessor')
   require_relative File.join('review', 'packagemetadataprocessor')
+  require_relative File.join('review', 'packagemanifestprocessor')
   require_relative File.join('review', 'packageprocessor')
   require_relative File.join('review', 'tableprocessor')
 end
--- lib/review/packageprocessor.rb
+++ lib/review/packageprocessor.rb
@@ -11,14 +11,25 @@ module UMPTG::Review
       fragments = super(args)
 
       metadata_processor = PackageMetadataProcessor.new
-      fragment_selector.containers = [ 'metadata' ]
+      manifest_processor = PackageManifestProcessor.new
 
       fragments.each do |fragment|
+        pck_version = fragment.node["version"]
+        case
+        when pck_version.nil?
+          fragment.review_msg_list << "Package Warning:  EPUB version not specified."
+        when pck_version[0] == '3'
+          fragment.review_msg_list << "Package INFO:     EPUB version is 3.x."
+        else
+          fragment.review_msg_list << "Package Warning:  EPUB version is #{pck_version}."
+        end
+
         fragment.has_elements.each do |elem_name, exists|
             fragment.review_msg_list << "Package INFO:  contains <#{elem_name}>." if exists
             fragment.review_msg_list << "Package INFO:  contains no <#{elem_name}>." unless exists
         end
 
+        fragment_selector.containers = [ 'metadata' ]
         meta_fragments = metadata_processor.process(
                   :name => args[:name],
                   :content => fragment.node.to_xml,
@@ -26,6 +37,18 @@ module UMPTG::Review
               )
         meta_fragments.each do |meta_frag|
           meta_frag.review_msg_list.each do |msg|
+            fragment.review_msg_list << "Package " + msg
+          end
+        end
+
+        fragment_selector.containers = [ 'manifest' ]
+        manifest_fragments = manifest_processor.process(
+                  :name => args[:name],
+                  :content => fragment.node.to_xml,
+                  :selector => fragment_selector
+              )
+        manifest_fragments.each do |man_frag|
+          man_frag.review_msg_list.each do |msg|
             fragment.review_msg_list << "Package " + msg
           end
         end
