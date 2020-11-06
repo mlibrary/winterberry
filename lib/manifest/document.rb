a564cfe79d18105f8ca831f95f1e12eaf9e1bea6
tbelc <34096219+tbelc@users.noreply.github.com>
2020-11-06 13:55:48 -0500

    Simplifications to the Manifest::Document class and update scripts process_monograph_epub and register_handles.

--- lib/manifest/document.rb
+++ lib/manifest/document.rb
@@ -1,6 +1,6 @@
 module UMPTG::Manifest
   class Document
-    attr_reader :name, :noid, :csv
+    attr_reader :name, :noid, :csv, :monograph_row, :isbn
 
     @@MONOGRAPH_FILE_NAME = '://:MONOGRAPH://:'
 
@@ -8,9 +8,6 @@ module UMPTG::Manifest
       @name = args[:name]
       csv_body = args[:csv_body]
       fail "Error: manifest is empty" if csv_body.nil? or csv_body.empty?
-
-      @monograph_row = nil
-      @isbns = {}
 
       begin
         @csv = CSV.parse(
@@ -22,13 +19,13 @@ module UMPTG::Manifest
       rescue Exception => e
         raise e.message
       end
-    end
 
-    def monograph_row
-      find_monograph_row()
-
-      do_raise("Error: no CSV monograph row") if @monograph_row == nil
-      return @monograph_row
+      @monograph_row = @csv.find {|row| row['file_name'] == @@MONOGRAPH_FILE_NAME }
+      @noid = @monograph_row['noid'] unless @monograph_row.nil?
+      @isbn = {}
+      unless @monograph_row.nil?
+        @isbn = parse_isbns(@monograph_row['isbn(s)'])
+      end
     end
 
     def representative_row(args = {})
@@ -36,16 +33,8 @@ module UMPTG::Manifest
 
       kind = args[:kind]
       row = @csv.find {|row| row['representative_kind'] == kind.downcase }
-      do_raise("Error: representative #{kind} not found") if row.nil?
+      raise "Error: representative #{kind} not found" if row.nil?
       return row
-    end
-
-    def isbn(args = {})
-      return nil unless args.has_key?(:format)
-
-      format = args[:format]
-      parse_isbns()
-      return @isbns[format]
     end
 
     def fileset(file_name)
@@ -63,41 +52,20 @@ module UMPTG::Manifest
              }
     end
 
-    def isbns
-      parse_isbns()
-      do_raise("Error: no isbns found for noid #{@noid}") if @isbns.nil? or @isbns.empty?
-      return @isbns
-    end
-
     private
 
-    def find_monograph_row
-      load()
-
-      # Find monograph row.
-      @monograph_row = @csv.find {|row| row['noid'] == @noid } unless @noid.nil?
-      @monograph_row = @csv.find {|row| row['file_name'] == @@MONOGRAPH_FILE_NAME } unless @csv_path.nil?
-    end
-
-    def parse_isbns
-      return unless @isbns.empty?
-
-      find_monograph_row()
-
+    def parse_isbns(isbns_property)
       isbns_property = @monograph_row['isbn(s)']
+      isbn_format = {}
       unless isbns_property.nil? or isbns_property.empty?
         isbns_list = isbns_property.split(';').each do |isbn|
           list = isbn.strip.downcase.match('([0-9\-]+)[ ]+\(([^\)]+)\)')
           unless list.nil?
-            @isbns[list[2]] = list[1]
+            isbn_format[list[2]] = list[1]
           end
         end
       end
-    end
-
-    def do_raise(msg)
-      raise "#{msg} for noid #{@noid}" unless @noid.nil?
-      raise "#{msg} for path #{File.basename(@csv_path)}" unless @csv_path.nil?
+      return isbn_format
     end
   end
 end
