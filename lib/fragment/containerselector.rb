0474be44eb88cd04a9db3af1cc09166913009acc
tbelc <34096219+tbelc@users.noreply.github.com>
2020-11-13 16:43:25 -0500

    Updated containerselector to handle attribute names/values. Updated collection.xsd to handle animated_gif. Updated script process_monograph_epub to use Fulcrum shared directory. Fixed error in script/process_monograph_resource_metadata where log message used wrong variable name.

--- lib/fragment/containerselector.rb
+++ lib/fragment/containerselector.rb
@@ -1,10 +1,30 @@
 module UMPTG::Fragment
   class ContainerSelector
 
-    attr_accessor :containers
+    attr_accessor :containers, :attribute_name, :attribute_values
+
+    def initialize()
+      @containers = []
+      @attribute_name = ""
+      @attribute_values = []
+    end
 
     def select_fragment(name, attrs = [])
-      return @containers.include?(name)
+      raise "Error: ContainerSelector - no containers specified." if @containers.empty?
+      
+      if @containers.include?(name)
+        return true if @attribute_name.strip.empty?
+
+        attrs_map = attrs.to_h
+        if attrs_map.key?(@attribute_name)
+          return true if @attribute_values.empty?
+
+          attrs_map[@attribute_name].strip.split(' ').each do |attrval|
+            return true if @attribute_values.include?(attrval)
+          end
+        end
+      end
+      return false
     end
   end
 end
