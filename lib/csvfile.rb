bd452f3bef37ac2342cb03aac0812cf019f6ce2f
Timothy W Belch <34096219+tbelc@users.noreply.github.com>
2022-12-06 13:47:20 -0500

    Support for FULCRUMOPS-7, importing PDFs.

--- lib/csvfile.rb
+++ lib/csvfile.rb
@@ -1,28 +1,31 @@
-require 'csv'
+module UMPTG
 
-class CSVFile
-  def self.read(args = {})
-    if args.has_key?(:csv_path)
-      csv_path = File.expand_path(args[:csv_path])
-      csv_body = File.read(csv_path)
-    else
-      csv_body = options[:csv_body]
+  require 'csv'
+
+  class CSVFile
+    def self.read(args = {})
+      if args.has_key?(:csv_path)
+        csv_path = File.expand_path(args[:csv_path])
+        csv_body = File.read(csv_path)
+      else
+        csv_body = options[:csv_body]
+      end
+      return nil if csv_body == nil or csv_body.empty?
+
+      CSV::Converters[:strip_field] = ->(value) { value.strip rescue value }
+      begin
+        csv_data = CSV.parse(
+                  csv_body,
+                  headers: true,
+                  converters: :strip_field,
+                  return_headers: false)
+       #          :header_converters => lambda { |h| h.downcase.gsub(' ', '_') })
+       #          :headers => true, :converters => :all,
+      rescue Exception => e
+        raise e.message
+      end
+
+      return csv_data
     end
-    return nil if csv_body == nil or csv_body.empty?
-
-    CSV::Converters[:strip_field] = ->(value) { value.strip rescue value }
-    begin
-      csv_data = CSV.parse(
-                csv_body,
-                headers: true,
-                converters: :strip_field,
-                return_headers: false)
-     #          :header_converters => lambda { |h| h.downcase.gsub(' ', '_') })
-     #          :headers => true, :converters => :all,
-    rescue Exception => e
-      raise e.message
-    end
-
-    return csv_data
   end
 end
