ba0db55af568f0a5978957b40117cad47c3b1c2c
Timothy W Belch <34096219+tbelc@users.noreply.github.com>
2024-04-15 15:24:11 -0400

    Add EPUBCheck 5.1.0.

--- lib/epubcheck.rb
+++ lib/epubcheck.rb
@@ -1,27 +1,58 @@
 module UMPTG
-  class EpubCheck
-    JAR_PATH = File.join(__dir__, 'epubcheck', 'epubcheck.jar')
-    CMD_STR_EPUB = "--version -q -o \"%s\" \"%s\""
-    CMD_STR_EPUBDIR = "--version -q -o \"%s\" -mode exp \"%s\""
+  class EPUBCheck
+    JAR_PATH = File.join(__dir__, 'epubcheck', 'v%s', 'epubcheck.jar')
+    OPTS_STR_VERSION = "--version -q"
+    OPTS_STR_EPUB = "-q -o \"%s\" \"%s\""
+    OPTS_STR_EPUBDIR = "-q -o \"%s\" -mode exp \"%s\""
     CMD_STR_ALL = "java -jar \"%s\" %s"
 
-    def self.check_file(args)
+    VERSIONS = [
+          "4.2.6",
+          "5.1.0"
+        ]
+
+    def self.check_file(args = {})
       epub_file = args[:epub_file]
       log_file = args[:logfile]
+      version = args[:version]
 
-      cmd_str = File.directory?(epub_file) ? CMD_STR_EPUBDIR : CMD_STR_EPUB
+      cmd_str = File.directory?(epub_file) ? OPTS_STR_EPUBDIR : OPTS_STR_EPUB
       java_cmd_str = sprintf(cmd_str, log_file, epub_file)
-      execute(java_cmd_str)
+      execute(
+            options: java_cmd_str,
+            version: version
+          )
     end
 
     def self.invoke(args = [])
       opts_str = args.count == 0 ? "-h" : args.join(' ')
-      execute(opts_str)
+      execute(
+            options: opts_str
+          )
     end
 
-    def self.execute(opts_str)
-      java_cmd_str = sprintf(CMD_STR_ALL, JAR_PATH, opts_str)
+    def self.execute(args = {})
+      opts_str = args[:options]
+      raise "missing options string" if opts_str.nil? or opts_str.strip.empty?
 
+      ver = args[:version]
+      ver = versions_default() if ver.nil? or ver.strip.empty?
+      jar_path = sprintf(JAR_PATH, ver)
+      raise "invalid EPUBCheck version #{jar_path}" unless File.file?(jar_path)
+
+      # Display the version
+      if ver[0] =="5"
+        # Version 5 will ignore the epub is version option is provided.
+        # So, display it first before evaluating the epub.
+        java_cmd_str = sprintf(CMD_STR_ALL, jar_path, OPTS_STR_VERSION)
+        system(java_cmd_str)
+      else
+        # Version 4 will display the version and evaluate
+        # the epub in the same call.
+        opts_str = "--version " + opts_str
+      end
+
+      java_cmd_str = sprintf(CMD_STR_ALL, jar_path, opts_str)
       ok = system(java_cmd_str)
       status = $?
 
@@ -29,6 +60,21 @@ module UMPTG
       when true
       else
       end
+    end
+
+    def self.versions
+      return VERSIONS
+    end
+
+    def self.versions_str(args = {})
+      sep = args[:separator]
+
+      sep = "|" if sep.nil? or sep.strip.empty?
+      return VERSIONS.join(sep)
+    end
+
+    def self.versions_default(args = {})
+      return VERSIONS.first
     end
   end
 end
