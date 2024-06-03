b083ae243df0d5199f5072b12ed3571d8f6e3d5e
Timothy W Belch <34096219+tbelc@users.noreply.github.com>
2024-06-03 10:33:40 -0400

    Updates for ticket fops0275.

--- lib/epub/rendition.rb
+++ lib/epub/rendition.rb
@@ -5,12 +5,15 @@ module UMPTG::EPUB
   class Rendition < UMPTG::Object
     attr_accessor :name, :opf_doc
 
+    #<spine toc="ncx"></spine>
     @@PKG_TEMPLATE = <<-PKG
     <?xml version="1.0" encoding="UTF-8"?>
     <package xmlns="http://www.idpf.org/2007/opf" version="3.0" unique-identifier="pub-id" dir="ltr" xml:lang="en">
-    <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:opf="http://www.idpf.org/2007/opf"></metadata>
+    <metadata xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:dcterms="http://purl.org/dc/terms/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:opf="http://www.idpf.org/2007/opf">
+    <dc:identifier id="pub-id">pubid</dc:identifier>
+    </metadata>
     <manifest></manifest>
-    <spine toc="ncx"></spine>
+    <spine></spine>
     </package>
     PKG
 
