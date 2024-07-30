fb83b4b889eae04667bc7107f25f04a52c9ab296
Timothy W Belch <34096219+tbelc@users.noreply.github.com>
2024-07-30 10:32:04 -0400

    Updates to support ticket fops0576.

---
+++ fops0576/embedlinkfilter.rb
@@ -0,0 +1,302 @@
+module UMPTG::Fulcrum::XResources::Filter
+
+  class EmbedLinkFilter < UMPTG::Fulcrum::Filter::ManifestFilter
+
+    EMBED_XPATH = <<-SXPATH
+    //*[
+    @data-fulcrum-embed-filename
+    ]
+    SXPATH
+
+    FIGURE_EMBED_XPATH = <<-SXPATH
+    //*[
+    local-name()='figure'
+    or @data-fulcrum-embed-filename
+    or (local-name()='img' and count(ancestor::*[local-name()='figure'])=0)
+    ]
+    SXPATH
+
+    FORMAT_STR = <<-FSTR
+    <p  data-fulcrum-embed-filename="%s"
+        data-fulcrum-embed-caption-field="%s"
+        data-fulcrum-embed-caption-link="%s"/>
+    FSTR
+
+    CLASS_FORMAT_STR = <<-CFSTR
+    <p class="%s"
+        data-fulcrum-embed-filename="%s"
+        data-fulcrum-embed-caption-field="%s"
+        data-fulcrum-embed-caption-link="%s"/>
+    CFSTR
+
+    def initialize(args = {})
+      args[:name] = :embed_link unless args.key?(:name)
+      process_figures = args[:process_figures]
+      process_figures = true if process_figures.nil?
+      args[:xpath] = process_figures ? FIGURE_EMBED_XPATH : EMBED_XPATH
+      super(args)
+    end
+
+    def create_actions(args = {})
+      name = args[:name]
+      reference_node = args[:reference_node]  # figure element
+
+      action_list = []
+
+      case reference_node.name
+      when "figure"
+        action_list = create_figure_actions(reference_node)
+      when "img"
+        action_list = create_img_actions(reference_node)
+      else
+        action_list = create_element_actions(reference_node)
+      end
+
+      return action_list
+    end
+
+    private
+
+    def create_figure_actions(reference_node)
+      fragment_node = reference_node.dup
+
+      action_list = []
+      caption_node = fragment_node.xpath("./*[local-name()='figcaption']").first
+
+      embed_file_name = fragment_node['data-fulcrum-embed-filename']
+      embed_file_name = embed_file_name.nil? ? "" : embed_file_name.strip
+
+      img_node_list = reference_node.xpath(".//*[local-name()='img']")
+
+      resource_node_list = []
+      resource_node_list << fragment_node unless embed_file_name.empty?
+      img_node_list.each do |img_node|
+        src = img_node["src"]
+        if src.nil? or src.strip.empty?
+          action_list << UMPTG::XML::Pipeline::Action.new(
+                   name: name,
+                   reference_node: reference_node,
+                   warning_message: "#{reference_node.name}: image with no @src value"
+               )
+          next
+        end
+        resource_node_list << img_node
+      end
+
+      resource_name_list = resource_node_list.collect do |r|
+        r["data-fulcrum-embed-filename"] if r.name == "figure"
+        r["src"] if r.name == "img"
+      end
+
+      if resource_node_list.count == 0
+        # No resource references found.
+        action_list << UMPTG::XML::Pipeline::Action.new(
+                 name: name,
+                 reference_node: reference_node,
+                 error_message: "#{reference_node.name}: no resource references found"
+             )
+      else
+        if resource_node_list.count > 1
+          # Multiple resource references found.
+          action_list << UMPTG::XML::Pipeline::Action.new(
+                   name: name,
+                   reference_node: reference_node,
+                   warning_message: "#{reference_node.name}: multiple resource references found #{resource_name_list.join(',')}"
+               )
+        end
+
+        resource_name_list = []
+        resource_node_list.each do |resource_node|
+          reference_name = resource_node.name == "img" ? \
+                    resource_node["src"] : \
+                    resource_node["data-fulcrum-embed-filename"]
+          resource_name = manifest.fileset_file_name(reference_name)
+          resource_name_list << resource_name unless resource_name.nil? or resource_name.strip.empty?
+        end
+
+        if resource_name_list.count > 0
+          # One or more resource references found.
+          caption_added = false
+          if caption_node.nil?
+            action_list << UMPTG::XML::Pipeline::Action.new(
+                     name: name,
+                     reference_node: reference_node,
+                     warning_message: \
+                       "#{reference_node.name}: added figcaption element for #{fragment_node}"
+                 )
+            fragment_node.add_child("<figcaption class=\"figcaption\"/>")
+            caption_node = fragment_node.xpath(".//*[local-name()='figcaption']").first
+            caption_added = true
+          end
+          block_list = caption_node.xpath(".//*[local-name()='p' or local-name()='div']")
+
+          caption_field = reference_node["data-fulcrum-embed-caption-field"]
+          caption_field = caption_field.strip.downcase unless caption_field.nil?
+          caption_link = reference_node["data-fulcrum-embed-caption-link"]
+          caption_link = (caption_link.strip.downcase == "true") unless caption_link.nil?
+
+          resource_name_list.each do |resource_name|
+            cf = caption_field.nil? ? "caption" : caption_field
+            cl = caption_link.nil? ? false : caption_link
+            if caption_added
+
+              if caption_field.nil? and caption_link.nil?
+                caption_node.add_child(CLASS_FORMAT_STR % ["default-media-display", resource_name, cf, "true"])
+                caption_node.add_child(CLASS_FORMAT_STR % ["enhanced-media-display", resource_name, cf, cl])
+              else
+                caption_node.add_child(FORMAT_STR % [resource_name, cf, cl])
+              end
+            else
+              block_list.each {|n| n.add_class("enhanced-media-display") }
+              caption_node.add_child(CLASS_FORMAT_STR % ["default-media-display", resource_name, cf, "true"])
+            end
+            fragment_node.remove_attribute("style")
+          end
+
+          caption_node.xpath(".//*[@data-fulcrum-embed-filename]").each do |node|
+            insert_metadata(node)
+          end
+          action_list << UMPTG::XML::Pipeline::Actions::MarkupAction.new(
+                   name: name,
+                   reference_node: reference_node,
+                   action: :replace_node,
+                   markup: fragment_node.to_xml,
+                   info_message: "#{reference_node.name}: links for resources #{resource_name_list.join(',')}"
+               )
+        end
+      end
+      return action_list
+    end
+
+    def create_element_actions(reference_node)
+      fragment_node = reference_node.dup
+
+      action_list = []
+      node = insert_metadata(fragment_node)
+      action_list << UMPTG::XML::Pipeline::Actions::MarkupAction.new(
+               name: name,
+               reference_node: reference_node,
+               action: :replace_node,
+               markup: node.to_xml,
+               info_message: "#{reference_node.name}: links for resources #{fragment_node.name}"
+           )
+      return action_list
+    end
+
+    def create_img_actions(reference_node)
+      # img element
+      reference_name = reference_node["src"]
+
+      action_list = []
+
+      content = manifest.fileset_title(reference_name)
+      unless content.empty?
+        content = "View Fulcrum resource"
+        link_markup = manifest.fileset_link_markup(
+                reference_name,
+                {
+                    description: content,
+                    download: manifest.fileset_allow_download(reference_name)
+                }
+              )
+
+        table_node = reference_node.xpath("./ancestor::*[local-name()='table'][1]").first
+        cell_cnt = table_node.nil? ? 0 : \
+            table_node.xpath("count(./descendant::*[local-name()='tr'][1]/*[local-name()='td'])")
+
+        if cell_cnt < 2
+          # Convert table to a figure
+          if cell_cnt == 0
+            p_node  = reference_node.xpath("./ancestor::*[local-name()='p'][1]").first
+            replace_node = p_node.nil? ? reference_node : p_node
+          else
+            section_node  = table_node.xpath("./ancestor::*[local-name()='section'][1]").first
+            replace_node = section_node.nil? ? table_node : section_node
+          end
+
+          figure_node = Nokogiri::XML::DocumentFragment.parse("<figure></figure>")
+          figure_node = figure_node.xpath("./*[local-name()='figure']").first
+          figure_node.add_child(reference_node)
+
+          link_container = UMPTG::XML::Pipeline::Actions::EmbedAction.default_container(reference_node, container = 'p')
+          link_container.add_child(link_markup)
+          figure_node.add_child("<figcaption>" + link_container.to_xml + "</figcaption>")
+
+          action_list << UMPTG::XML::Pipeline::Actions::MarkupAction.new(
+                   name: name,
+                   reference_node: replace_node,
+                   action: :replace_node,
+                   markup: figure_node.to_xml,
+                   info_message: "#{reference_name}: replace #{replace_node.name} with figure"
+               )
+        elsif cell_cnt > 1
+          # Add link to cell caption
+          cell_ndx = reference_node.xpath("count(./ancestor::*[local-name()='td'][1]/preceding-sibling::*)")
+          cell_node = reference_node.xpath("./ancestor::*[local-name()='tbody'][1]/*[local-name()='tr' and position()=last()]/*[local-name()='td' and position()=#{cell_ndx+1}]").first
+
+          unless cell_node.nil?
+            cell_class = cell_node.first_element_child["class"]
+            markup = "<p class=\"#{cell_class}\">" + link_markup + "</p>"
+
+            action_list << UMPTG::XML::Pipeline::Actions::MarkupAction.new(
+                     name: name,
+                     reference_node: cell_node,
+                     action: :add_child,
+                     markup: markup,
+                     info_message: "#{reference_name}: links for resource"
+                 )
+            action_list << UMPTG::XML::Pipeline::Actions::SetAttributeValueAction.new(
+                     name: name,
+                     reference_node: reference_node,
+                     attribute_name: "data-fulcrum-embed",
+                     attribute_value: "false",
+                     info_message: "#{reference_name}: don't embed resource"
+                 )
+          end
+        end
+      end
+      return action_list
+    end
+
+    def insert_metadata(node)
+      resource_name = node["data-fulcrum-embed-filename"]
+      field = node["data-fulcrum-embed-caption-field"]
+      #link = node["data-fulcrum-embed-caption-link"]
+      link = node.name == "span" ? "true" : node["data-fulcrum-embed-caption-link"]
+
+      case field
+      when "caption"
+        content = manifest.fileset_caption(resource_name)
+      when "title"
+        content = manifest.fileset_title(resource_name)
+      else
+        content = node.inner_html
+      end
+
+      if link == "true"
+        markup = manifest.fileset_link_markup(
+                resource_name,
+                {
+                    description: content,
+                    download: manifest.fileset_allow_download(resource_name)
+                }
+              )
+      else
+        markup = content
+      end
+
+      node.remove_attribute("data-fulcrum-embed-filename")
+      node.remove_attribute("data-fulcrum-embed-caption-field")
+      node.remove_attribute("data-fulcrum-embed-caption-link")
+
+      case node.name
+      when "span"
+        node = Nokogiri::XML::DocumentFragment.parse(markup)
+      else
+        node.add_child(markup)
+      end
+      return node
+    end
+
+  end
+end
