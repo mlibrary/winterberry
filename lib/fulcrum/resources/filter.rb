ffd891987c0e728d909938fe5e495582d9cc4207
Timothy W Belch <34096219+tbelc@users.noreply.github.com>
2025-03-05 17:12:17 -0500

    Updates to support ticket fops0549, added filter updatehref.

--- lib/fulcrum/resources/filter.rb
+++ lib/fulcrum/resources/filter.rb
@@ -4,13 +4,15 @@ module UMPTG::Fulcrum::Resources
   require_relative(File.join("filter", "removefigcaptionfilter"))
   require_relative(File.join("filter", "resourcereferencefilter"))
   require_relative(File.join("filter", "updatealttextfilter"))
+  require_relative(File.join("filter", "updatehreffilter"))
 
   FILTERS = {
       embed_link: Filter::EmbedLinkFilter,
       fulcrum_css: Filter::FulcrumCSSFilter,
       remove_figcaption: Filter::RemoveFigcaptionFilter,
       resource_reference: Filter::ResourceReferenceFilter,
-      update_alt: Filter::UpdateAltTextFilter
+      update_alt: Filter::UpdateAltTextFilter,
+      update_href: Filter::UpdateHREFFilter
     }
 
   def self.EmbedLinkFilter(args = {})
@@ -21,11 +23,19 @@ module UMPTG::Fulcrum::Resources
     return FILTERS[:fulcrum_css].new(args)
   end
 
+  def self.RemoveFigCaptionFilter(args = {})
+    return FILTERS[:remove_figcaption].new(args)
+  end
+
   def self.ResourceReferenceFilter(args = {})
     return FILTERS[:resource_reference].new(args)
   end
 
   def self.UpdateAltTextFilter(args = {})
     return FILTERS[:update_alt].new(args)
+  end
+
+  def self.UpdateHREFFilter(args = {})
+    return FILTERS[:update_href].new(args)
   end
 end
---
+++ lib/fulcrum/resources/filter/updatehreffilter.rb
@@ -0,0 +1,58 @@
+module UMPTG::Fulcrum::Resources::Filter
+
+  class UpdateHREFFilter < UMPTG::Fulcrum::Filter::ManifestFilter
+
+    XPATH = <<-SXPATH
+    //*[
+    local-name()='a'
+    ]
+    SXPATH
+
+    def initialize(args = {})
+      args[:name] = :embed_link
+      args[:xpath] = XPATH
+      super(args)
+    end
+
+    def create_actions(args = {})
+      name = args[:name]
+      reference_node = args[:reference_node]  # <a> element
+
+      raise "unknown element #{reference_node.name}" unless reference_node.name == 'a'
+
+      action_list = []
+
+      href = reference_node["href"]
+      if href.start_with?('https://www.fulcrum.org/concern/file_sets/')
+        fileset_noid = href.delete_prefix('https://www.fulcrum.org/concern/file_sets/')
+        fileset = manifest.fileset_from_noid(fileset_noid)
+        if fileset["noid"].empty?
+          action_list << UMPTG::XML::Pipeline::Action.new(
+                   name: name,
+                   reference_node: reference_node,
+                   warning_message: \
+                     "#{reference_node.name}: no Fulcrum fileset for #{href}"
+               )
+        else
+          action_list << UMPTG::XML::Pipeline::Actions::SetAttributeValueAction.new(
+                   name: name,
+                   reference_node: reference_node,
+                   attribute_name: "href",
+                   attribute_value: fileset["doi"],
+                   warning_message: \
+                     "#{reference_node.name}: found Fulcrum fileset #{href}"
+               )
+        end
+      else
+          action_list << UMPTG::XML::Pipeline::Action.new(
+                   name: name,
+                   reference_node: reference_node,
+                   info_message: \
+                     "#{reference_node.name}: found link #{href}"
+               )
+      end
+
+      return action_list
+    end
+  end
+end
